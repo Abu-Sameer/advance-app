b4d6791652bbcb1ae00b0c9d2f0fb0a427e9a2f1
Abu-Sameer <adesoyeabolajiyusuph@gmail.com>
2023-05-25 12:04:26 +0100

    Todo App tutoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import UserCart from './component/UserCart';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <UserCart />
     </div>
   );
 }
-
-export default App;
